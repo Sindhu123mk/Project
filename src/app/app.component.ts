4b78c0e2901711aad7f0b61a26d097bd49d3828a
a <b>
2021-12-15 09:58:27 +0530

    pay

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,22 +8,14 @@ import { Component } from '@angular/core';
   <div style="padding:5px">
   <ul class="nav nav-tabs">
    <li> <a routerLink="login" routerLinkActive="active"> Login </a></li>
+   <li> <a routerLink="registration" routerLinkActive="active"> Registration</a></li>
    <li> <a routerLink="home" routerLinkActive="active"> Home</a></li>
    <li> <a routerLink="profile" routerLinkActive="active">Profile</a></li>
    <li> <a routerLink="busavailable" routerLinkActive="active">BusesAvailable</a></li>
    <li> <a routerLink="ticketbookings" routerLinkActive="active">Tickets Bookings</a></li>
    <li> <a routerLink="upcommingbuses" routerLinkActive="active">Upcomming Buses</a></li>
    <li> <a routerLink="logout" routerLinkActive="active">Logout</a></li>
-   <li> <a routerLink="signup" routerLinkActive="active">Sign Up</a></li>
   
-
-
-
-
-
-
-
-
 
   </ul>
   <router-outlet></router-outlet>
